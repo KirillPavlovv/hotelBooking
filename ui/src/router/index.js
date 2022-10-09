2dd3d84ac8fcd6a92adddc35f96be1102411e58e
Kirill <kirillp1988@gmail.com>
2022-10-09 15:14:34 +0300

    vue project preparation

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,20 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import RoomsView from '../views/RoomsView.vue'
+import RoomView from "@/views/RoomDetailsView";
+import Rooms from "@/views/RoomsView";
+import ReservationView from "@/views/ReservationView";
+import ReservationsListView from "@/views/ReservationsListView";
+import NotFound from "@/views/NotFound";
 
 Vue.use(VueRouter)
 
-const router = new VueRouter({
-  mode: 'history',
-  base: import.meta.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: RoomsView
-    },
+const routes = [
+  {
+    path: '/',
+    redirect: '/rooms'
+  },
+  {
+    path: '/rooms',
+    name: 'rooms',
+    component: Rooms,
+    children: [
+      {
+        path: '/room',
+        name: 'room',
+        component: RoomView,
+      },
+    ]
+  },
+  {
+    path: '/reservation',
+    name: 'reservation',
+    component: ReservationView
+  },
+  {
+    path: '/all-reservations',
+    name: 'reservations',
+    component: ReservationsListView
+  },
+  {
+    path: '/:not.Found(.*)',
+    name: 'reservations',
+    component: NotFound
+  }
+]
 
-  ]
+const router = new VueRouter({
+  routes,
+  // mode: 'history'
 })
 
 export default router
