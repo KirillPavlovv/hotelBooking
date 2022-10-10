420eb4fdd14d001e2537c227ade023f3afad6a01
Kirill <kirillp1988@gmail.com>
2022-10-10 23:26:45 +0300

    refactoring

--- ui/src/components/base/BaseButton.vue
+++ ui/src/components/base/BaseButton.vue
@@ -1,7 +1,7 @@
 <template>
-<button v-if="!link" class="mode">
-  <slot></slot>
-</button>
+  <button v-if="!link" class="mode">
+    <slot></slot>
+  </button>
   <router-link v-else :to="to" class="mode">
     <slot></slot>
   </router-link>
